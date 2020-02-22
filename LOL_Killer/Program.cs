e6cdcfde1459cc0d46debd269e8e2aa2902247f3
frogview <jhongbo@163.com>
2020-02-22 23:47:54 +0800

    可以将LOL_Killer固定在菜单栏或者开始菜单，当英雄联盟卡死时，按Window键显示菜单栏，启动程序，直接强制结束LOL进程。

---
+++ LOL_Killer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOL_Killer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            KillProcess();
+        }
+
+        public static void KillProcess()//关闭线程
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var p in processes)
+            {
+                if (p.ProcessName.ToLower().Contains("league"))
+                {
+                    Console.WriteLine(p.ProcessName);
+                    if (p.ProcessName == "League of Legends")
+                    {
+                        try
+                        {
+                            p.Kill();
+                            p.WaitForExit(); // possibly with a timeout
+                        }
+                        catch (Win32Exception e)
+                        {
+                            Console.WriteLine(e.Message.ToString());  // process was terminating or can't be terminated - deal with it
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message.ToString()); // process has already exited - might be able to let this one go
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
